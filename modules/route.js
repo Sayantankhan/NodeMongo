7d02ff9d8e77b25a2815b63f41d7b8944d1c8e08
Sayantankhan <iamstk14@gmail.com>
2017-11-18 20:11:02 +0530

    module/route.js modified

--- modules/route.js
+++ modules/route.js
@@ -1,16 +1,44 @@
-//require for the routing purpose
 var express = require('express');
-var app = express();
-var router = express.Router();
+var router  = express.Router();
+var mongocon = require('./mongocon');
 
-// Home page route
-router.get('/', function (req, res) {
-  console.log('root');
+router.use(function(req, res, next) {
+	// log each request to the console
+	console.log(req.method, req.url);
+	// continue doing what we were doing and go to the route
+	next();
 });
 
-// About page route
-router.get('/about', function (req, res) {
-  console.log('about');
+// home page route (http://localhost:8080)
+router.get('/', function(req, res) {
+	res.send('im the home page!');
+});
+
+// about page route (http://localhost:8080/about)
+router.get('/about', function(req, res) {
+	res.send('im the about page!');
+	var email = 'iamstk14@gmail.com';
+	var password = '123456';
+	mongocon.retriveData(email,password);
+	
+});
+
+// route middleware to validate :name
+router.param('name', function(req, res, next, name) {
+	// do validation on name here
+	// blah blah validation
+	// log something so we know its working
+	console.log('doing name validations on ' + name);
+	// once validation is done save the new item in the req
+	req.name = name;
+	// go to the next thing
+	next();
+});
+
+// route with parameters (http://localhost:8080/hello/:name)
+router.get('/hello/:name', function(req, res) {
+	res.send('hello ' + req.name + '!');
+	//res.send('hello ' + req.params.name + '!');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -24,10 +24,11 @@
 var express = require('express');
 var app     = express();
 var port    = 	process.env.PORT || 8082  ;
-var mongocon = require('./modules/mongocon');
 
 // ROUTES
 // ==============================================
+var router = require('./modules/route');
+
 
 // sample route with a route the way we're used to seeing it
 app.get('/sample', function(req, res) {
@@ -35,53 +36,7 @@ app.get('/sample', function(req, res) {
 });
 
 // we'll create our routes here
-
-// get an instance of router
-var router = express.Router();
-
-// route middleware that will happen on every request
-router.use(function(req, res, next) {
-	// log each request to the console
-	console.log(req.method, req.url);
-	// continue doing what we were doing and go to the route
-	next();
-});
-
-// home page route (http://localhost:8080)
-router.get('/', function(req, res) {
-	res.send('im the home page!');
-});
-
-// about page route (http://localhost:8080/about)
-router.get('/about', function(req, res) {
-	res.send('im the about page!');
-	var email = 'iamstk14@gmail.com';
-	var password = '123456';
-	mongocon.retriveData(email,password);
-	
-});
-
-// route middleware to validate :name
-router.param('name', function(req, res, next, name) {
-	// do validation on name here
-	// blah blah validation
-	// log something so we know its working
-	console.log('doing name validations on ' + name);
-	// once validation is done save the new item in the req
-	req.name = name;
-	// go to the next thing
-	next();
-});
-
-// route with parameters (http://localhost:8080/hello/:name)
-router.get('/hello/:name', function(req, res) {
-	res.send('hello ' + req.name + '!');
-	//res.send('hello ' + req.params.name + '!');
-});
-
-// apply the routes to our application
 app.use('/', router);
-
 
 // login routes
 app.route('/login')
